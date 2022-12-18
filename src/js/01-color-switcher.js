dd86355391726c6d9e3576c42b7c05e982f0f7f8
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2022-12-18 10:13:23 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+// console.log(btnStart);
+// console.log(btnStop);
+let timerId = null;
 
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(colorChang, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
+
+function colorChang() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
